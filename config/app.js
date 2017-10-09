d4b76ce87df81d50fdc53b1b52310321d766273d
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-10-09 21:55:39 +0500

    same db

--- config/app.js
+++ config/app.js
@@ -4,8 +4,8 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'https://door-quote-automator.herokuapp.com/';
-// export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'http://localhost:5000';
+// export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'https://door-quote-automator.herokuapp.com/';
+export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'http://localhost:5000';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
 
