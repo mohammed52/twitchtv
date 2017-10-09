4147d50b6fadeb562abc43a77dce8dce8bc43eae
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2017-10-09 15:22:41 +0500

    updated mongodb url for heroku

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,5 @@
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
+// export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
+export const db = process.env.MONGOHQ_URL
 
 export default {
   db
