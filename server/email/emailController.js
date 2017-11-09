b1adf931aaf949cdd135a9833694950b1dd1bdb6
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2017-11-09 16:18:41 +0500

    wip: how to use existing db controller methods in email controller in the server???

--- server/email/emailController.js
+++ server/email/emailController.js
@@ -5,7 +5,7 @@ import SelectedOptions from '../db/mongo/models/selectedOptionsModel';
 import nodemailerHelper from './nodemailerHelper'
 import { controllers } from '../db';
 
-const selectedOptionsController = controllers && controllers.selectedOptionsController;
+// const selectedOptionsController = controllers && controllers.selectedOptionsController;
 
 // send email
 export function sendEmail(req, res) {
@@ -29,25 +29,49 @@ export function sendEmail(req, res) {
 export function sendQuote(req, res) {
   console.log("MAP sendQuote is working");
 
-  function resolveAfter2Seconds(x) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        console.log("x", x);
-        resolve(x);
-      }, 2000);
-    });
+
+  async function getData(req, res) {
+    const response = await controllers.selectedOptionsController.all(req, res);
+    console.log("response", response);
+    return response
   }
 
+  getData(req, res).then((res) => {
+    console.log('MAP RESPONSE RECEIVED')
+    console.log("res", res);
+    let mailOptions = {
+      from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+      to: 'mohammed.petiwala52@gmail.com', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'MEK', // plain text body
+      html: 'MEK' // html body
+    };
 
-
-  const a = resolveAfter2Seconds(20);
-  console.log("a", a);
-
-  async function add(x) {
-    const a = resolveAfter2Seconds(20);
-    console.log("a", a);
-
+    nodemailerHelper(mailOptions)
+    return res.redirect('/')
   }
+  );
+
+
+  // function resolveAfter2Seconds(x) {
+  //   return new Promise(resolve => {
+  //     setTimeout(() => {
+  //       console.log("x", x);
+  //       resolve(x);
+  //     }, 2000);
+  //   });
+  // }
+
+
+
+  // const a = resolveAfter2Seconds(20);
+  // console.log("a", a);
+
+  // async function add(x) {
+  //   const a = resolveAfter2Seconds(20);
+  //   console.log("a", a);
+
+  // }
 
   // add(10).then((v) => {
   //   console.log("v", v);
@@ -55,44 +79,13 @@ export function sendQuote(req, res) {
 
 
 
-  async function add1(x) {
-    const a = await resolveAfter2Seconds(20);
-    const b = await resolveAfter2Seconds(30);
-    return x + a + b;
-  }
+  // async function add1(x) {
+  //   const a = await resolveAfter2Seconds(20);
+  //   const b = await resolveAfter2Seconds(30);
+  //   return x + a + b;
+  // }
 
-add1(10).then(v => {
-  console.log(v); // prints 60 after 4 seconds.
-});
-
-
-  return controllers.selectedOptionsController.all(req, res)
-
-  // .then((res) => {
-
-  //   console.log("res");
-  // })
-  // .catch((res) => {
-  //   console.log("res");
-  // })
-
-
-
-  // const all = controllers.selectedOptionsController.all
-
-  // all(req, res)
-  //   .then((selectedOptions) => {
-
-  //     let mailOptions = {
-  //       from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-  //       to: 'mohammed.petiwala52@gmail.com', // list of receivers
-  //       subject: 'Hello ✔', // Subject line
-  //       text: selectedOptions[0].contactInfo.companyName, // plain text body
-  //       html: selectedOptions[0].contactInfo.companyName // html body
-  //     };
-
-  //     nodemailerHelper(mailOptions)
-
-//     return selectedOptions
-//   });
+// add1(10).then(v => console.log(v) // prints 60 after 4 seconds.
+// );    
+// })
 }
--- server/email/nodemailerHelper.js
+++ server/email/nodemailerHelper.js
@@ -27,6 +27,7 @@ export default (mailOptions) => {
       console.log('Message sent: %s', info.messageId);
       // Preview only available when sending through an Ethereal account
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      console.log("info", info);
 
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
